810ceb1376755e25ed452786258c79b4ef139093
SantanuDas2345 <90392244+SantanuDas2345@users.noreply.github.com>
2023-01-09 19:57:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect, useRef} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+  const inp1 = useRef();
+  const [text,setText] = useState('');
+  const clickHandler = ()=> {
+    setText(inp1.current.value);
+  }
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' ref={inp1}></input>
+      <button id='button' onClick={clickHandler}>Click</button>
+      <p id='text'> Hello my name is {text?text:'__'} and I study at Newton School</p>
     </div>
   )
 }
